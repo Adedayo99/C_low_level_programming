0b02390dbcdf6606b36791d8f9bda43bc0a8eed6
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-10-15 10:49:15 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+/* printing lower case alphabet*/
+	int ch;
+	ch = 97;
+	while(ch <= 'z')
+{
+	putchar(ch);
+	ch++;
+}
+
+
+	return (0);
+}
