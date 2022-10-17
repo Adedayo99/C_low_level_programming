e423bcdf8c6b8f51d3a626f8cc4eed908d8c5aa2
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-10-17 03:50:34 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+
+#include<stdio.h>
+
+int main(void)
+
+{
+
+	int ch;
+
+	ch = '1';
+	while (ch <= '9')
+{
+	putchar(ch);
+	ch++;
+}
+
+	return (0);
+
+}
