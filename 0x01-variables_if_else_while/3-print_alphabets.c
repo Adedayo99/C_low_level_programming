98ea2a7e1eed3524d414582f9c0a757eaa916255
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-10-17 01:58:26 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+
+#include<stdio.h>
+
+
+int main(void)
+
+{
+	int ch;
+
+	ch = 67;
+
+	while (ch < 'Z')
+{
+	putchar(ch);
+	ch++;
+}
+
+
+	return (0);
+}
