171d19f73f7a18b78fd9cde94a1e69df62a2bc1c
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-10-17 03:10:05 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+
+#include<stdio.h>
+
+int main(void)
+
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+{
+	printf("%d", i);
+
+
+
+
+
+
+	return (0);
+}
