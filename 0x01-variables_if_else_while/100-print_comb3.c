ce6197868d8ee2a4f0b6e0448bf0eeed5bb570a7
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-10-17 05:04:13 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+
+#include<stdio.h>
+
+int main(void)
+
+{
+
+
+	int i;
+	int j;
+
+	for (i='0'; i<='9'; i++):
+{
+	for (j='1'; j<='9'; j++):
+{
+	if (i == j)
+	;
+	else
+{
+	putchar('i');
+	putchar('j');
+	putchar(',');
+	putchar(' ');
+}
+}
+
+}
+
+	putchar('\n');
+	return (0);
+}
+
