85caa676c1bf78efdcd72764bd7ad4f0981b4132
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-10-17 04:04:40 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+
+#include<stdio.h>
+
+int main()
+{
+
+	int ch;
+	int i;
+
+	ch = '0';
+	i = 97;
+
+	while (ch <= '9')
+{
+	putchar(ch);
+	ch++;
+}
+
+	while (i <= 102)
+{
+	putchar(i);
+	i++;
+}
+	putchar('\n');
+	return (0);
+}
