3a93c05b2566acfc4cf57bffa6aab5e7836bf4cb
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-10-19 05:09:05 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+/**
+* is_lower - checks for lowercase characters
+*
+* Return: Always 1 (success)
+*/
+
+#include "main.h"
+
+int is_lower(int c)
+{
+	int i;
+	int flag;
+
+	i = 'a';
+	while (i <= 'z')
+{
+	if (c == i)
+	flag = 1;
+	return (1);
+}
+
+	if (flag == 1)
+	return (0);
+
+}
