036420ed0595b45fa3eed1e03b21981e326743aa
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-10-19 07:35:57 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,39 @@
+/**
+* _isalpha.c - checks for alphabetic character
+*@c: The character to examine
+*
+* Return: Always 1 (success)
+*/
+
+#include "main.h"
+
+int _isalpha(int c)
+{
+	int i;
+	int flag;
+	int j;
+
+	i = 'a';
+	j = 'A';
+
+	while (i <= 'z')
+{
+	i++;
+	if (i == c)
+	flag = 1;
+}
+
+	while (j <= 'Z')
+{
+	j++;
+	if (j == c)
+	flag = 1;
+}
+
+	if (flag == 1)
+	return (1);
+
+	else
+	return (0);
+}
+		
