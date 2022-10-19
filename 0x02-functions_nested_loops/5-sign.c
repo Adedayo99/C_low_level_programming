190272e8f5294d4616c149d421b3b66930774e1b
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-10-19 08:44:05 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+/**
+*print_sign - checks the sign of a number
+*@n: the number whole sign is checked
+*
+*Return: Always 0 (success)
+*/
+
+include "main.h"
+
+int print_sign(int n)
+{
+	if (n > 0)
+{
+	return (1);
+	_putchar(43);
+}
+
+	if (n == 0)
+{
+	return (0);
+	_putchar(48);
+}
+
+	if (n < 0)
+{
+	return (-1);
+	_putchar(45);
+}
+
+}
