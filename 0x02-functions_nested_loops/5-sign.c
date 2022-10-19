4a91e3a02cecbb454c277d14dd384afa2a04392b
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-10-19 12:27:24 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,23 +9,23 @@
 
 int print_sign(int n)
 {
-
+	
 	if (n > 0)
-{
-	return (1);
-	_putchar(43);
-}
+	{
+		return (1);
+		_putchar(43);
+	}
 
-	if (n == 0)
-{
-	return (0);
-	_putchar(48);
-}
+	else if (n == 0)
+	{	
+		return (0);
+		_putchar(48);
+	}
 
-	if (n < 0)
-{
-	return (-1);
-	_putchar(45);
-}
+	else 
+	{
+		return (-1);
+		_putchar(45);
+	}
 
 }
