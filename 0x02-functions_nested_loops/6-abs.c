42e3ebb96d2639010a88f4627dd223124b1365ea
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-10-19 13:14:40 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+/**
+* _abs - Obtains abs val of input num
+*@int: type of expected input
+*
+* Return: Always 0 (success)
+*/
+
+#include "main.h"
+
+int _abs(int)
+{
+
+	if (int > 0)
+		int = int;
+
+	else
+		int = -int;
+}
