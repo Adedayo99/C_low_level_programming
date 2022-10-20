fa562fc9763d2e1da5e79415560062d62792048f
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-10-20 11:31:34 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,27 @@
+/**
+* print_most_numbers - prints some nums
+*@void: no input
+*
+* Return: Always 0 (success)
+*/
+
+#include "main.h"
+
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i <= '9')
+{
+	if((i == '2') || (i == '4'))
+	;
+
+	else
+	_putchar(i);
+
+	i++;
+}
+	_putchar('\n');
+}
