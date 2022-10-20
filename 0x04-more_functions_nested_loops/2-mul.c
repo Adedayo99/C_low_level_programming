44d48da9178000eee9e6b498cf73c41506aed1ed
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-10-20 11:02:53 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+/**
+* mul - multiplies two ints
+*@a,b: inputs to multiply
+*
+*Return: Always 0 (success)
+*/
+
+#include"main.h"
+
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a*b;
+	return (prod);
+}
