56f7a6d7e9c8adf92946b982b1ffa0a421926965
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-10-20 11:11:37 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+/**
+* print_numbers - lists nums
+*
+* Return: Always 0 (success)
+*/
+
+#include "main.h"
+
+void print_numbers(void)
+{
+	int i;
+
+	i = '0';
+	while (i <= '9')
+{
+	_putchar(i);
+}
+	_putchar('\n');
+}
