7cdbcd678da126c38c05488234f0e778dd486715
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-10-24 03:07:49 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+/**
+* swap_int - swaps input values
+*@a: input variables
+*@b: input var
+*Return: No return value
+*/
+
+#include "main.h"
+
+void swap_int(int *a, int *b)
+{
+
+	int swap;
+
+	swap = *a;
+	a = *b;
+	b =  swap;
+}
