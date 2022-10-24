c0ca82b3b76152b8de010cef3c02627b493f4768
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-10-24 09:00:57 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+/**
+* puts_half - prints half of input string
+*@str: input string
+*
+* Return: No return val
+*/
+
+#include "main.h"
+
+void puts_half(char *str)
+{
+	int i;
+	int count;
+
+	count = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	count += 1;
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	if (count % 2 == 0)
+		{
+		for (count = count / 2; str[count] != '\0'; count++)
+		_putchar(str[count]);
+		}
+	else
+		{
+		for (count = (count - 1) / 2; str[count] != '\0'; count++)
+		_putchar(str[count + 1]);
+		}
+
+	}
+	_putchar('\n');
+
+}
+
+
