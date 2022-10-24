fa32af87a12123816ad7ce630752cdcc70d4c6eb
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-10-24 04:30:20 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+/**
+* print_rev - Reversed string printing
+*@s: character strinf input
+*
+*Return: No retun val
+*/
+
+#include "main.h"
+
+void print_rev(char *s)
+{
+	int i;
+	int count;
+
+	count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	count += 1;
+	}
+
+	for (i = count; i >= 0; i--)
+	{
+	_putchar(s[i]);
+	}
+
+	_putchar('\n');
+
+}
