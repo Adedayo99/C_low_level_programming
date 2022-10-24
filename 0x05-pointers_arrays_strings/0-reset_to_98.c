99463e71a38bf82764ac715f107d3cfc793654aa
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-10-24 02:52:54 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+/**
+* reset_to_98 - changes pointer var value
+*@*n: pointer argument to function
+* 
+* Return: Returns var with new value
+*/
+
+#include "main.h"
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+	return (*n);
+}
