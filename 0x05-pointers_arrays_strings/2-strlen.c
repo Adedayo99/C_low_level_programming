de968d174b2de83b7afc006d0cc04268c3d0c59b
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-10-24 03:56:47 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,26 @@
+/**
+* _strlen - counts string len
+*@s: input char string pointer
+*
+*Return: An int value
+*/
+
+#include "main.h"
+
+int _strlen(char *s)
+{
+	int i;
+	int count;
+
+	count = 0;
+
+
+	for (i = 0; s[i] != '\0'; i++)
+	}
+
+		count += 1;
+	}
+
+	return (count);
+}
+
