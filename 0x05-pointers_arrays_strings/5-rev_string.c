0034f9a1acef21a6a87b4281124e5ffe2dcc4d2c
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-10-24 05:01:03 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+/**
+* rev_string - string reversal f
+*@s: input string
+*
+* Return: No return val
+*/
+
+#include "main.h"
+
+void rev_string(char *s)
+{
+	int i;
+	int count;
+	char mychar[];
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	count += 1;
+	}
+
+	for (i = --count; i >= 0; i--)
+	{
+	mychar[i] = s[i];
+	}
+
+	s = mychar[];
+
+}
