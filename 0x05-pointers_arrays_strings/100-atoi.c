da32ba3082f156a3de7bdd96fac675a76b047597
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-10-25 11:25:08 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,19 @@
+/**
+* _atoi - function for casting to int
+*@s: inout var
+*
+*Return: int value returned
+*/
+
+#include "main.h"
+#include "stdio.h"
+
+int _atoi(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+	printf("%c", s[i]);
+	}
+}	
