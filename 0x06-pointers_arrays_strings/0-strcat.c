423f6d9992bfa2512e3cf3ca2595368a766bf838
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-10-27 03:13:10 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+/**
+* _strcat - concatenates strings
+*@dest: destination var
+*@src: source var
+*
+* Return: Char pointer
+*/
+
+#include "main.h"
+
+char *_strcat(char *dest, char *src);
+{
+	int i = 0;
+	int count = 0;
+
+	while (*dest != '\0')
+	{
+	count++;
+	}
+
+	count = count - 1;
+
+	for (i = 0; *src != '\0'; i++, count++)
+	{
+	*(dest + count) = *(src + i);
+	}
+
+	*dest = '\0';
+	return (dest);
+	_putchar('\n');
+}
