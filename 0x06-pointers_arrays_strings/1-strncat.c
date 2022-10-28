06f4efc42735b21dfd9a35c7664029ad2be5ae86
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-10-28 08:28:20 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,42 @@
+
+/**
+* _strcat - concatenates strings
+*@dest: destination var
+*@src: source var
+*@n: number of bytes
+*
+* Return: Char pointer
+*/
+
+#include "main.h"
+
+char *_strcat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int count1 = 0;
+	int count2 = 0;
+
+	while (*dest != '\0')
+	{
+	count1++;
+	dest++;
+	}
+
+	while (*src != '\0')
+	{
+	count2++;
+	src++;
+	}
+
+	for (; i<= n; i++, count1++)
+	{
+	*(dest + count1) = *(src + i);
+	}
+	*dest = '\0';
+
+	return (dest);
+	_putchar('\n');
+}
+
+
+
