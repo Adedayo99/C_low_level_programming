edf7166deecc36964db7afca4c312827b81de35b
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-10-28 08:44:59 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+/*
+*_strncpy - concatenates strings
+*@dest: destination var
+*@src: source var
+*@n: number of bytes
+*
+* Return: Char pointer
+*/
+
+#include "main.h"
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (; i <= n; i++)
+	{
+	*(dest + i) = *(src + i);
+	}
+
+	*dest = '\0';
+	return (dest);
+}
