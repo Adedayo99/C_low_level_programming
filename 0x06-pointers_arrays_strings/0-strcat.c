8d4cacf0c47cdb82361fc3a24742284d3e09a93a
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-10-28 07:43:15 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,13 +14,13 @@ char *_strcat(char *dest, char *src)
 	int count1 = 0;
 	int count2 = 0;
 
-	while (*dest != '\0')
+	while (dest != '\0')
 	{
 	count1++;
 	dest++;
 	}
 
-	while (*src != '\0')
+	while (src != '\0')
 	{
 	count2++;
 	src++;
