6304dd09b358c7efd7a5595410239777667f3d5d
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-10-31 10:36:30 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,6 @@
 /**
 * reverse_array - reverses an array of int
-*@*a: pointer to int array
+*@a: pointer to int array
 *@n: number of ints to swap
 *
 * Return: none
@@ -13,9 +13,11 @@ void reverse_array(int *a, int n)
 	int *ptr = a;
 	int i;
 	int temp;
-	int j = ((int) sizeof(a) / (int) sizeof(a[0])) - 1;
+	int j = ((int) sizeof(a) / (int) sizeof(ptr[0])) - 1;
 
-	for (i = 0; i < n; i++)
+
+
+	for (i = 0; i <= n; i++)
 	{
 
 		temp = ptr[i];
