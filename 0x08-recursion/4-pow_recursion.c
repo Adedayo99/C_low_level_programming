efb90152f0d26504e531bf93b4de6c1071ca366e
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-02 05:29:34 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,30 @@
+/**
+* _pow_recursion - finds power of number recursively
+*@x: base number
+*@y: exponent number
+*
+* Return: int val
+*/
+
+#include "main.h"
+
+int _pow_recursion(int x, int y)
+{
+
+	int result = 0;
+
+	if (y < 0)
+	result = -1;
+
+	if (y = 0)
+	result = 1;
+
+	if (y > 0)
+	{
+		x = x * _pow_recursion(x, y - 1);
+		result = x;
+	}
+
+	return (result);
+
+
