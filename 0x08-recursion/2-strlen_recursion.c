a24f6a66e5247a6cebc9f29f63de418a3b2c3e95
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-02 04:46:00 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,24 @@
+/**
+* _strlen_recursion - returns length of string
+*@s: input string
+*
+* Return: an int val
+*/
+
+#include "main.h"
+
+int _strlen_recursion(char *s)
+{
+	char *ptr = s;
+	int i = 0;
+
+	if (*ptr != '\0')
+	{
+	i++;
+	_strlen_recursion(ptr + 1);
+	}
+
+
+	return (i);
+	_putchar('\n');
+}
