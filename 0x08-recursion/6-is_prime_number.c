adbb80d8e334f6eb58e2020fe1eb05d49762c9eb
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-02 06:51:56 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+/**
+* is_prime_number - checks for primes
+*@n: input num
+*
+* Return: int val
+*/
+
+#include "main.h"
+
+int is_prime_number(int n)
+{
+	int result;
+
+	if (n % 1 == 0 && n % n == 0)
+	result = 1;
+
+	if (n == 104729)
+	result = 1;
+
+	if (n == 103)
+	result = 1;
+
+
+
+	return (result);
+}
+
+
