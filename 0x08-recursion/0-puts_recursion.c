c92eb479b6dc80c177e642ae9a84a929290899c1
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-02 04:07:14 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+/**
+* _puts_recursion - prints string
+*@s: pointer to input string
+*
+* Return: No return val
+*/
+
+#include "main.h"
+
+void _puts_recursion(char *s)
+{
+	char *ptr = s;
+
+	if (*ptr == '\0')
+	;
+
+	else
+	{
+	_putchar(*ptr);
+	_puts_recursion(ptr + 1);
+	}
+}
