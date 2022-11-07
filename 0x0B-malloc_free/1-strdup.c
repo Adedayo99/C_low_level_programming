81b57efa42cefff5a020cfad964d00be5dff0daf
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-07 07:45:47 -0800

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,46 @@
+/**
+* _strdup - duplicates a string
+*@str: input string
+*
+* Return: char pointer
+*/
+
+#include "main.h"
+#include "stdlib.h"
+
+char *_strdup(char *str)
+{
+	int i = 0;
+	char *ptr1 = str;
+	char *ptr_arr;
+	int len = 0;
+	int flag = 0;
+
+	while(ptr1[i] != '\0')
+	{
+	len++;
+	i++;
+	}
+
+
+	ptr_arr = (char *) malloc(sizeof(char) * (len + 1));
+
+	if (ptr_arr == NULL)
+	flag = 0;
+
+	else
+	for(i = 0; i < len; i++)
+	{
+	ptr_arr[i] = ptr1[i];
+	}
+
+	flag = 1;
+
+
+	if (flag == 1)
+	return (ptr_arr);
+
+	else
+	return (NULL);
+
+}
