5fd90f46bf28832095139937bf3a6862538c6c8f
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-07 06:12:35 -0800

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+/**
+* create_array - creates array with variable aize
+*@size: num of bytes
+*@c: initial char
+*
+*Return: Pointer or NULL
+*/
+
+#include "main.h"
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	int flag = 0;
+
+	if (size > 0)
+	{
+	ptr = malloc(sizeof(char) * (int) size);
+	ptr[0] = c;
+	flag = 1;
+	return (ptr);
+
+	}
+
+	if (size == 0)
+	return (NULL);
+}
