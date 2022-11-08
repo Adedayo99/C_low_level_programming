2cf52437e93078f776a71b6044fe3daa6272b90f
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-08 02:41:17 -0800

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,45 +11,50 @@
 #include "stddef.h"
 char *str_concat(char *s1, char *s2)
 {
-	char *ptr1 = s1;
-	char *ptr2 = s2;
 	int i = 0;
 	int j;
 	int flag = 0;
 
-	int len;
+	int len, len1, len2;
 	char *ptr_arr;
 
 	if (ptr1 && ptr2 != NULL)
-{
+	{
 	flag = 1;
-	for (i = 0; ptr1[i] != '\0'; i++)
-	;
+	len1 = len_check(s1);
+	len2 = len_check(s2);
 
-	for (j = 0; ptr2[j] != '\0'; j++)
-	;
-
-	len = i + j + 2;
+	len = len1 + len2 + 1;
 	ptr_arr = (char *) malloc(sizeof(char) * len);
 
-	for (i = 0, j = 0;  ptr1[j] != '\0'; i++, j++)
-	{
-		ptr_arr[i] = ptr1[j];
-	}
+	for (i = 0; i < len1; i++)
+	ptr_arr[i] = s1[i];
 
-	for (i = i + 1, j = 0; ptr2[j] != '\0'; i++, j++)
-	ptr_arr[i] = ptr2[j];
-
+	for (i = len1, j = 0; i < len - 3 && s2[j] != '\0'; i++, j++)
+	ptr_arr[i] = s2[j];
 
 	ptr_arr[i] = '\0';
+	}
 
-}
 	if (flag == 1)
-	return (ptr1);
+	return (ptr_arr);
 
 	else
 	return (NULL);
 
 
 
-	}
+}
+
+
+int len_check(char *str)
+{
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+	;
+
+	len += 1;
+
+	return (len);
+}
