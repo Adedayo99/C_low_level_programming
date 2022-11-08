221f86a91476703ca5063b9e74dceb69bd7aa627
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-08 06:37:58 -0800

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+/**
+* alloc_grid - initializes 2D array
+*@width: col num
+*@height: row num
+*
+* Return: pointer ti array in heap
+*/
+
+int **alloc_grid(int width, int height)
+{
+
+	int **ptr;
+	int flag;
+	int i, j;
+
+	if (width && height > 0)
+	{
+	flag = 1;
+
+	ptr = (int *) malloc(sizeof(int *) * height);
+
+	for (i = 0; i < height; i++)
+	ptr[i] = (int *) malloc(sizeof(int) * width);
+
+		for (i = 0; i < height; i++)
+		{
+		for (j = 0; j < width; j++)
+		ptr[i][j] = 0;
+		}
+
+	}
+
+	if (flag == 1)
+	return (ptr);
+
+	else
+	return (NULL);
+
