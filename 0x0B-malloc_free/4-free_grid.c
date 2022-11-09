8e31605fd12b08bd397c4dd1db617e363bfbabf1
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-09 01:22:52 -0800

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+/**
+* free_grid - free allocated memory
+*@grid: pointer to 2D array
+*@height: height of the grid
+*
+* Return: No return val
+*/
+
+void free_grid(int **grid, int height)
+{
+
+	int i;
+	int **mgrid = grid;
+
+	if (mgrid != NULL)
+	for (i = 0; i < height; i++)
+	free(mgrid[i]);
+}
