477ed84078ab93c357699e89da7117b60e652c1a
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-09 04:37:18 -0800

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -12,17 +12,17 @@ char *string_arrange(char *str);
 
 char **strtow(char *str)
 {
-
+	char *pp = str
 	char *ptr;
 	char **p;
 	int i, j;
 
-	if (str == NULL || str == " ")
+	if (str == NULL || pp == "")
 	return (NULL);
 
 	ptr = string_arrange(str);
 
-	**p = (char **) malloc(sizeof(char) * 3);
+	p = (char **) malloc(sizeof(char) * 3);
 
 	for (i = 0; i < 3; i++)
 	p[i] = (char *) malloc(sizeof(char) * 10);
@@ -36,12 +36,18 @@ char **strtow(char *str)
 	return (p);
 }
 
+/**
+* string_arrange - orders string
+*@ss: input
+*
+* Return: pointer
+*/
 
-
-char *string_arrange(char *str)
+char *string_arrange(char *ss)
 {
 	int i, len = 0;
 	char *ptr;
+	char *str = ss;
 
 	while(str[i] != '\0')
 	{
