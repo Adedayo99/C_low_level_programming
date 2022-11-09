5a31b7d3d29cd345706bb068d6f8009750cd6df8
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-09 09:59:51 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+/**
+* main - prints its name
+*@argc: count ofbinouts
+*@argv: inouts
+*
+* Return: int val
+*/
+
+#include "main.h"
+
+int main(int argc char *argv[])
+{
+	int i;
+
+	for (i = 0; argv[0][i] != '\0'; i++)
+	_putchar(arg[0][i]);
+}
