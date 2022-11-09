070ea94db4f319d8da6206ff2bd73d9650700c6b
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-09 10:58:58 -0800

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,14 +8,13 @@
 
 #include "main.h"
 #include <unistd.h>
+#include <stdio.h>
 int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; argv[0][i] != '\0'; i++)
-	_putchar(argv[0][i]);
-
-	_putchar('\n');
+	for (i = 0; i < argc; i++)
+	printf("%s\n",argv[i]);
 	return (0);
 }
 
