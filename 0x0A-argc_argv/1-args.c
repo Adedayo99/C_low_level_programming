4e7bb083b28c7ca09416250e28f9cf79435ea254
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-09 11:12:43 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+/**
+* main - main function
+*@argc: count of vars
+*@argv: pointer to strings
+*
+* Return: int val
+*/
+
+#include "main.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
