00e37c8e9d4c34f79a5dbddcb14dfd67c43812f1
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-10 09:14:40 -0800

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+/**
+* malloc_checked - dynamic memory allocator
+*@b: num of blocks to allocate
+*
+* Return: non
+*/
+
+#include "main.h"
+#include <stdlib.h>
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc((int) b);
+
+	if (ptr == NULL)
+	return (98);
+
+	else
+	return (ptr);
+
+}
