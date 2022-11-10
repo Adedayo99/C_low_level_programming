e8f75a0e8ed2e149e8b3b4296d16b1e0374ec08c
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-10 00:52:46 -0800

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,11 +9,16 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-int main(int argc)
+int main(int argc, char *argv[])
 {
+	int flag = 0;
+
 	printf("%d\n", argc);
 
+	if (flag == 0)
+	return (argv[0]);
 
+	else
 	return (0);
 
 
