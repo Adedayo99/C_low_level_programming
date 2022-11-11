3522d13bd21a67e42d998a114dd15b3f3e8fad11
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-11 01:12:06 -0800

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,11 +14,15 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	int i, j, len = 0;
+	int len2 = 0;
 
 	char *ptr;
 
 	while (s1[i] != '\0')
 	i++, len++;
+
+	while (s2[j] != '\0')
+	j++, len2++;
 
 	len = len + n + 1;
 	ptr = (char *) malloc(sizeof(char) * len);
@@ -28,6 +32,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i = 0; s1[i] != '\0'; i++)
 	ptr[i] = s1[i];
+
+	if ((int) n >= len2)
+	n = len2;
 
 	for (i = i, j = 0; j < (int) n; j++, i++)
 	ptr[i] = s2[j];
