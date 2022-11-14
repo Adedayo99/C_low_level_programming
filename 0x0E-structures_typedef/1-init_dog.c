9887514f35c23702113050b3113d97a3609c0d91
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-14 06:39:50 -0800

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+/**
+* init_dog - function to initialize dog
+*@d: pointer to specific dog var
+*@name: name of dog
+*@age: age of dog
+*@owner: owner of dog
+*
+* Return: non
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+}
+
