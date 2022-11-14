d2d4c308f3c3a9d65063f1234a20dc76f4da72cf
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-14 03:43:12 -0800

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,12 @@
 /**
 * main - only functiin present
-*@argc: unused
-*@argv: to obtain progran name
-* 
+*
+*
+*
 * Return: 0
 */
 
 #include <stdio.h>
-#include <stddef.h>
-#include <stdlib.h>
 int main(void)
 {
 	printf("%s\n", __FILE__);
