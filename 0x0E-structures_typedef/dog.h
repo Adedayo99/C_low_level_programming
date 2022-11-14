8e85e0e793ce32fe7dce71d27812a68082cef2da
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-14 06:29:57 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
+/**
+* struct dog - a rule to generate composite vars for dogs
+*@name : name ppt
+*@age: age ppt
+*@owner: owner ppt
+*/
+
+struct dog 
+{
 	char *name;
 	float age;
 	char *owner;
