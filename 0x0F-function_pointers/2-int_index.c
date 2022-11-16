a5de43016952ff6cacc05ef5f8f932e719dcb70e
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-16 03:04:09 -0800

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+/**
+* int_index - function to run other func checks on ints
+*@array: pointer to input array
+*@size: length of array
+*@cmp: pointer to instructions to be run
+*
+* Return: an Int val
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int result;
+	int flag = 0;
+
+	if (size <= 0)
+	return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		result = (*cmp)(array[i]);
+
+		if (result != 0)
+		{
+		flag = 1;
+		return (i);
+		}
+	}
+
+	if (flag == 0)
+	return (-1);
+
+}
