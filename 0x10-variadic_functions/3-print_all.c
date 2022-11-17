8c8593ca4a3eccf41374fd954ecb5338259c296f
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-17 13:36:00 -0800

    3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,62 @@
+/**
+* print_all - prints ints and strings
+*@format: input formats
+*
+* Return: non
+*/
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+
+void print_all(const char * const format, ...)
+{
+
+	int i = 0;
+	int len = 0;
+
+	char ch;
+	int in;
+	float fl;
+	char *str;
+	va_list args;
+
+	va_start(args, format);
+
+	while (format[i] != "\0")
+	len++, i++;
+
+	i = 0;
+
+	while (i <= len)
+	{
+		if (format[i] == c)
+		{
+		ch = va_arg(args, char);
+		printf("%c ", ch);
+		}
+
+		else if (format[i] == i)
+		{
+		in = va_arg(args, int);
+		printf("%d ", in);
+		}
+
+		else if (format[i] == f)
+		{
+		fl = va_arg(args, float);
+		printf("%f ", fl);
+		}
+
+		else if (format[i] == s)
+		{
+		str = va_arg(args, char *);
+		if (str == NULL)
+		printf("(nil)");
+		else
+		printf("%s ", str);
+		}
+
+		i++;
+	}
+	printf("\n");
+}
