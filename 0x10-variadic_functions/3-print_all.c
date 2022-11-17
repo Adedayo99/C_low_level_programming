b9b0dbfc359fb9983c39842935d9be05a8c62d2e
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-17 13:58:49 -0800

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,40 +14,40 @@ void print_all(const char * const format, ...)
 	int i = 0;
 	int len = 0;
 
-	char ch;
+	int ch;
 	int in;
-	float fl;
+	double fl;
 	char *str;
 	va_list args;
 
 	va_start(args, format);
 
-	while (format[i] != "\0")
+	while (format[i] != '\0')
 	len++, i++;
 
 	i = 0;
 
 	while (i <= len)
 	{
-		if (format[i] == c)
+		if (format[i] == 'c')
 		{
-		ch = va_arg(args, char);
+		ch = va_arg(args, int);
 		printf("%c ", ch);
 		}
 
-		else if (format[i] == i)
+		else if (format[i] == 'i')
 		{
 		in = va_arg(args, int);
 		printf("%d ", in);
 		}
 
-		else if (format[i] == f)
+		else if (format[i] == 'f')
 		{
-		fl = va_arg(args, float);
+		fl = va_arg(args, double);
 		printf("%f ", fl);
 		}
 
-		else if (format[i] == s)
+		else if (format[i] == 's')
 		{
 		str = va_arg(args, char *);
 		if (str == NULL)
