b3f17291aa98b2b826e96a16acb08520601daa8e
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-27 07:20:18 -0800

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+/**
+* print_list - a function that prints the data in a linked list
+*@h: pointer to head of linked list
+*
+* Return: thr number of nodes
+*/
+#include <stdio.h>
+#include "lists.h"
+
+
+
+size_t print_list(const list_t *h);
+{
+	int i = 0;
+
+	list_t *ptr = h;
+
+	while (ptr != NULL)
+	{
+
+		count++;
+		if ((*ptr).str != NULL)
+		printf("[%lu] %s", (*ptr).len, (*ptr).data);
+
+		else
+		printf("[0] (nil)");
+
+		ptr = (*ptr).next;
+	}
+
+	return (count);
+}
