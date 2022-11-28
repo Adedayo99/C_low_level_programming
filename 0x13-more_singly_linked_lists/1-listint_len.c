3e3fa9aa84b955d1eb8d59c3dd70678d2ffc6971
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-28 03:41:25 -0800

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+/**
+* listint_len - length of int linked list
+*@h: head pointer to linked list
+*
+* Return: size_t value
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+	const listint_t *ptr = h;
+
+	while (ptr != NULL)
+	{
+		count++;
+
+		ptr = (*ptr).next;
+	}
+
+	return (count);
+}
