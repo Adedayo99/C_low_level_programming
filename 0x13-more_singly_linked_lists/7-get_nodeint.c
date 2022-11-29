afc2fc4adab38ac57c1edb92da95d62534338889
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-29 05:55:10 -0800

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -19,7 +19,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		temp = temp->next;
 		if (temp == NULL)
-		return (null);
+		return ((listint_t *) NULL);
 
 		count++;
 	}
