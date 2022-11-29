751737b6edec4717f734ef588e031d8df0547064
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-29 01:54:34 -0800

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+/**
+* free_list2 - frees linked list and assigns head as NULL
+*@head: pointer to head pointer of list
+*
+* Return: void
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp, *temp2;
+
+	temp = *head;
+
+	while (temp =! NULL)
+	{
+		temp2 = temp->next;
+		free(temp);
+		temp = temp2;
+	}
+
+	*head = NULL;
+}
