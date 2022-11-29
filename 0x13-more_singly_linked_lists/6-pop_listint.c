29a9917555becef8efa464140d53c8104f10b4f0
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-11-29 02:40:51 -0800

    6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+/**
+* pop_listint - deletes head node and returns its data
+*@head: pointer to head pointer of linked list
+*
+* Return: data of deleted node
+*/
+
+int pop_listint(listint_t **head)
+{
+	int a;
+
+	if (*head == NULL)
+	return (0);
+
+	a = (*head)->n;
+	free((**head));
+
+	return (a);
+
+}
