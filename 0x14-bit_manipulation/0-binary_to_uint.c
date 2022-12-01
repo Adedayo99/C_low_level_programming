52358a5743a330afc82278238cbb58653a354087
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-12-01 03:36:05 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,7 +4,23 @@
 *
 * Return: output
 */
+#include "main.h"
+#include <stdlib.h>
 
 unsigned int binary_to_uint(const char *b)
 {
-	
+
+	int i;
+	unsigned int var = 0;
+
+	if (b == NULL || !b)
+	return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		var += atoi(b[i]) * (2 ** i);
+	}
+
+	return (var);
+
+}
