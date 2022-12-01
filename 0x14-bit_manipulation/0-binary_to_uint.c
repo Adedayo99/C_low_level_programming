6802ab380bacfbadecfdc2b51896239a8fa1cae5
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-12-01 03:01:58 -0800

    update

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,10 @@
+/**
+* binary_to_uint - function to convert binary to unsigned int
+*@b: pointer to string set of 1s and 0s
+*
+* Return: output
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	
