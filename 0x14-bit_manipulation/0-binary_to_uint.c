a99d3baba695987b403970337a284d13d129c076
Adedayo99 <ajibadeadedayo99@gmail.com>
2022-12-02 04:00:50 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,21 +10,24 @@
 unsigned int binary_to_uint(const char *b)
 {
 
-	int i;
-	unsigned int var = 0;
+	int i, j;
+	int  var = 0;
 	int expo = 1;
 
 	if (b == NULL || !b)
 	return (0);
 
-	for (i = 0; b[i] != '\0'; i++)
-	{
+	for (j = 0; b[j] != '\0'; j++)
 
-		var += ((int) b[i]) * expo;
-	
-		expo = expo * 2;
+	for (i = j; i >= 0; i--)
+	{
+		if (b[i] == '0' || b[i] == '1')
+		{
+			var += ((int) b[i] * expo); 
+			expo = expo * 2;
+		}
 	}
 
-	return (var);
+	return ((unsigned int) var);
 
 }
